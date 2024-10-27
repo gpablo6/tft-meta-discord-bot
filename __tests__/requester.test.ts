bdeaeb093b021cfd6a8fbd054c3b4a9b6b487199
Dytox <rc19106@ues.edu.sv>
2024-10-26 21:43:40 -0600

    test modified

--- __tests__/requester.test.ts
+++ __tests__/requester.test.ts
@@ -2,13 +2,31 @@ import { sampleGet } from "../src/index";
 
 
 test("test samppleGet is expected", async () => {
-    const result = await sampleGet();
-    expect(result.recipes).toHaveLength(10);
-    result.recipes.forEach(element => {
-        expect(element).toMatchObject({
-            id: expect.any(Number),
-            name: expect.any(String)    
-        })
-        
-    });
+    try {
+        const result = await sampleGet();
+        expect(result.recipes).toHaveLength(10);
+        let i = result.recipes[0].id;
+        console.log("Initial id # : "+i);
+        result.recipes.forEach(element => {
+            expect(element).toMatchObject({
+                id: i++,
+                name: expect.any(String) 
+           })
+                
+        });
+
+        console.log("Last id # : "+ --i);
+    } catch (error) {
+        console.log(error);        
+    }
+});
+
+test(" Recipes skipped ", async () => {
+    try{
+        const result = await sampleGet();
+        expect(result.skip).toBe(5);
+        console.log("Quantity recipes skipped : " + result.skip);
+    }catch(error){
+        console.log(error);
+    }   
 });
